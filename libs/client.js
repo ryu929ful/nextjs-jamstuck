6dc1c85ce07668136df8ee340e32fdef1954fd8f
fujii_ryu <>
2024-12-25 10:21:33 +0900

    done

--- libs/client.js
+++ libs/client.js
@@ -1,7 +1,7 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-    serviceDomain:process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
-    apiKey:process.env.NEXT_PUBLIC_API_KEY
+    serviceDomain:"blogservicee",
+    apiKey:"416hRGqCPxzzNLrsAWvlZQ58NpWWjX7wtHCN"
 });
 
